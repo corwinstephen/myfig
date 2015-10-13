39caceb82d02bd758258fbf3d7216d29b2b440dc
Stephen Corwin <corwinstephen@gmail.com>
2015-10-13 11:33:29 -0400

    change password

--- config/initializers/comfortable_mexican_sofa.rb
+++ config/initializers/comfortable_mexican_sofa.rb
@@ -103,8 +103,8 @@ end
 
 # Default credentials for ComfortableMexicanSofa::AccessControl::AdminAuthentication
 # YOU REALLY WANT TO CHANGE THIS BEFORE PUTTING YOUR SITE LIVE
-ComfortableMexicanSofa::AccessControl::AdminAuthentication.username = 'username'
-ComfortableMexicanSofa::AccessControl::AdminAuthentication.password = 'password'
+ComfortableMexicanSofa::AccessControl::AdminAuthentication.username = ENV['ADMIN_LOGIN']
+ComfortableMexicanSofa::AccessControl::AdminAuthentication.password = ENV['ADMIN_PASSWORD']
 
 # Uncomment this module and `config.admin_auth` above to use custom admin authentication
 # module ComfyAdminAuthentication
