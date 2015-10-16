f7ababbb6d92ccaa4f1d64c22bfe06524fb97e2c
Stephen Corwin <corwinstephen@gmail.com>
2015-10-16 14:17:24 -0400

    Update box link color

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,14 +1,5 @@
 module ApplicationHelper
   def activeclass(tab)
-    active = case tab
-      when 'about'
-        @cms_page.try(:slug) == 'about'
-      when 'getting_around'
-        @cms_page.try(:slug) == 'getting_around'
-      when 'businesses'
-        @cms_page.try(:slug) == 'businesses'
-    end
-
-    'active' if active
+    'active' if @cms_page.try(:slug) == tab
   end
 end
